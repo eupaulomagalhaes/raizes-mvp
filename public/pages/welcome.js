95186729727a9270bc289ee4dddb826b4d7770c5
Paulo Magalhães <eupaulomagalhaes@gmail.com>
2025-11-05 17:00:15 -0300

    ui(welcome): badge 72vw radius 12.5%, grid 2 col com padding 8.45vw; DON à esquerda, balão+CTAs à direita

--- public/pages/welcome.js
+++ public/pages/welcome.js
@@ -12,18 +12,18 @@ export default {
           </div>
         </div>
 
-        <div class="welcome-don">
-          <img src="https://storage.googleapis.com/flutterflow-io-6f20.appspot.com/projects/raizes-m-v-p-i9jdtd/assets/5m2imr90fuaf/inteiro.png" alt="DON" style="width:100%; height:auto;" />
-        </div>
-
-        <div class="welcome-bubble">
-          <div class="speech">Eu me chamo DON, e vou te ajudar!</div>
-        </div>
-
-        <div class="cta-area">
-          <a class='btn' data-variant='primary' href='#/register'>Começar</a>
-          <a class='cta-link' href='#/login'>Já tenho uma conta!</a>
-        </div>
+        <section class="welcome-bottom">
+          <div class="welcome-don">
+            <img src="https://storage.googleapis.com/flutterflow-io-6f20.appspot.com/projects/raizes-m-v-p-i9jdtd/assets/5m2imr90fuaf/inteiro.png" alt="DON" />
+          </div>
+          <div class="welcome-right">
+            <div class="welcome-bubble"><div class="speech">Eu me chamo DON, e vou te ajudar!</div></div>
+            <div class="cta-area">
+              <a class='btn' data-variant='primary' href='#/register'>Começar</a>
+              <a class='cta-link' href='#/login'>Já tenho uma conta!</a>
+            </div>
+          </div>
+        </section>
       </main>
     `;
   },
